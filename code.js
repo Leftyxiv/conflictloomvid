78e36a47f9faaf0993dcfc1f3b5c34746a2ddd85
antonio ledoux <aledoux@dropbox.com>
2022-02-21 08:59:42 -0500

    first one down

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 // 1) return the sum of all numbers in the input array
 const sum = (input) => {
-
+    return input.reduce((acc, curr) => acc + curr, 0);
 };
 
 // 2) return the sum of all numbers up to the given input
