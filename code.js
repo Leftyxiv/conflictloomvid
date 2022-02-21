93444827fec3bc825876982d5c2d96191d0c3604
antonio ledoux <aledoux@dropbox.com>
2022-02-21 09:05:06 -0500

    numba 2

--- code.js
+++ code.js
@@ -5,6 +5,11 @@ const sum = (input) => {
 
 // 2) return the sum of all numbers up to the given input
 const sumTo = (input) => {
-
+    if (input === 0) {
+        return 0;
+    } else {
+        return sumTo(input - 1);
+    }
 };
+
 
