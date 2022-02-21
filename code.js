582d899872f7c07ffa498656af2ee300b2c07f7f
leftyxiv <manny@aledoux.net>
2022-02-21 08:48:24 -0500

    pushing it up

---
+++ code.js
@@ -0,0 +1,10 @@
+// 1) return the sum of all numbers in the input array
+const sum = (input) => {
+
+};
+
+// 2) return the sum of all numbers up to the given input
+const sumTo = (input) => {
+
+};
+
